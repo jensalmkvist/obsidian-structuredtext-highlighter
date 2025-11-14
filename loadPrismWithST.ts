03f9fad7d2d4be168e81a3f4014e0b1922f98b09
Jens Thieme Almkvist <jens@axmanautomation.com>
2025-11-14 15:49:24 +0100

    added more keywords

--- loadPrismWithST.ts
+++ loadPrismWithST.ts
@@ -1,8 +1,48 @@
 import { loadPrism } from "obsidian";
 
+
+
 const applyPrismST = (Prism: any) => {
+
+  const controlKeywordGroups = {
+
+    ifStatementKeywords: ['IF', 'ELSIF', 'ELSE', 'THEN', 'END_IF'],
+    caseStatementKeywords: ['CASE', 'OF', 'END_CASE'],
+    forStateMentKeywords: ['FOR', 'TO', 'BY', 'DO', 'END_FOR'],
+    whileStatementKeywords: ['WHILE', 'DO', 'END_WHILE'],
+    repeatStatmentKeywords: ['REPEAT', 'UNTIL', 'END_REPEAT'],
+    continueKeyword: ['CONTINUE'],
+    exitStatementKeywords: ['EXIT'],
+    returnStatementKeywords: ['RETURN']
+  };
+
+  const typedKeywordGroups = {
+    intKeywords: ['USINT', 'SINT', 'UINT', 'INT', 'UDINT', 'DINT', 'ULINT', 'LINTT'],
+    floatKeywords: ['REAL', 'LREAL'],
+    boolKeywords: ['BOOL'],
+    stringKeywords: ['STRING', 'WSTRING'],
+    binKeywords: ['BYTE', 'WORD', 'DWORD', 'LWORD'],
+    timeKeywords: ['TIME', 'LTIME']
+  };
+
+  const declarationKeywords = {
+    variables: ['VAR', 'VAR_INPUT', 'VAR_OUTPUT', 'VAR_IN_OUT', 'VAR_TEMP', 'VAR_CONSTANT', 'END_VAR'],
+    types: ['TYPE', 'END_TYPE', 'STRUCT', 'END_STRUCT'],
+    functions: ['FUNCTION', 'END_FUNCTION', 'FUNCTION_BLOCK', 'END_FUNCTION_BLOCK']
+  };
+
+  const allKeywords = [
+    ...Object.values(controlKeywordGroups).flat(),
+    ...Object.values(typedKeywordGroups).flat(),
+    ...Object.values(declarationKeywords).flat()
+  ];
+
+  const keywordRegex = new RegExp(`\\b(${allKeywords.join('|')})\\b`);
+
   Prism.languages.structuredtext = Prism.languages.extend('clike', {
-    keyword: /\b(IF|THEN|ELSE|END_IF|FOR|TO|DO|END_FOR|WHILE|END_WHILE|CASE|OF|END_CASE|TYPE|END_TYPE|STRUCT|END_STRUCT|BOOL|INT|REAL)\b/,
+
+
+    keyword: keywordRegex,
     boolean: /\b(TRUE|FALSE)\b/,
     operator: /\b(AND|OR|NOT|XOR)\b|:=|<>|<=|>=|<|>/,
     number: /\b\d+(\.\d+)?\b/,
